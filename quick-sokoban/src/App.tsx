ea9dfbe97e22d90fa4fecbecd61016df0d8c9354
Noah May <noahmouse2011@gmail.com>
2023-06-05 05:04:16 -0500

    quick-sokoban: Add image assets and basic grid display
    So much for quick

--- quick-sokoban/src/App.tsx
+++ quick-sokoban/src/App.tsx
@@ -1,9 +1,45 @@
 import "./App.css";
 
+const puzzle = `
+WWWW
+WPEW
+WBBW
+WWWW
+`;
+
+const gridImageMap = {
+    b: "/images/sokoban-block.png",
+    e: "/images/sokoban-empty.png",
+    p: "/images/sokoban-player.png",
+    w: "/images/sokoban-wall.png",
+} as const;
+
+const grid: string[][] = puzzle
+    .trim()
+    .split("\n")
+    .map((row) =>
+        row
+            .trim()
+            .toLowerCase()
+            .split("")
+            .map((key) => gridImageMap[key as keyof typeof gridImageMap]),
+    );
+
 function App() {
+    console.log(grid);
     return (
         <>
-            <div>init</div>
+            <div
+                style={{
+                    display: "grid",
+                    gridTemplateColumns: "repeat(4, 64px [col])",
+                    gridTemplateRows: "repeat(4, 64px [row])",
+                }}
+            >
+                {grid.flat().map((src) => (
+                    <img src={src} style={{ imageRendering: "pixelated", scale: "4" }} />
+                ))}
+            </div>
         </>
     );
 }
