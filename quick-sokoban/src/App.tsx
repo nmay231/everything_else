332e606fead526393a2fda7c930ec8cd8824db36
Noah May <noahmouse2011@gmail.com>
2023-06-05 21:41:14 -0500

    quick-sokoban: GameState object

--- quick-sokoban/src/App.tsx
+++ quick-sokoban/src/App.tsx
@@ -1,44 +1,119 @@
+import { useMemo } from "react";
 import "./App.css";
+
+const CELL_SIZE = 64; // Pixel-length of images
+
+const gridNameMap = {
+    b: "block",
+    e: "empty",
+    p: "player",
+    w: "wall",
+    g: "goal",
+} as const;
+
+type GridSlot = (typeof gridNameMap)[keyof typeof gridNameMap];
+type Coord = [number, number];
+
+function indexToCoord(index: number, width: number): Coord {
+    return [Math.floor(index / width), index % width];
+}
+
+class GameState {
+    w: number;
+    h: number;
+    grid: GridSlot[][];
+    player: Coord;
+    // blocks: Coord[];
+    goals: Coord[];
+
+    constructor(game: string) {
+        if (!game.trim()) {
+            throw Error("game string empty");
+        }
+
+        this.grid = game
+            .trim()
+            .split("\n")
+            .map((row) =>
+                row
+                    .trim()
+                    .toLowerCase()
+                    .split("")
+                    .map((x) => gridNameMap[x as keyof typeof gridNameMap]),
+            );
+
+        const widths = new Set(this.grid.map((row) => row.length));
+        if (widths.size != 1) {
+            throw Error("row lengths must be the same");
+        }
+
+        // Why the heck do sets not come with a pop method...
+        this.w = widths.values().next().value as number;
+        this.h = this.grid.length;
+
+        const flatGrid = this.grid.flat();
+        let player = null as null | Coord;
+        this.goals = [];
+        let blocks = 0;
+
+        for (const [i, value] of flatGrid.entries()) {
+            if (value === "goal") {
+                this.goals.push(indexToCoord(i, this.w));
+            } else if (value === "player") {
+                if (player) {
+                    throw Error("multiple players found, not allowed since you have no friends D:");
+                }
+                player = indexToCoord(i, this.w);
+            } else if (value === "block") {
+                blocks += 1;
+            }
+        }
+
+        if (!player) {
+            throw Error("Must provide player");
+        } else {
+            this.player = player;
+        }
+        if (!this.goals.length) {
+            throw Error("Must have at least one goal");
+        }
+        if (blocks < this.goals.length) {
+            throw Error("Must have more blocks than goals");
+        }
+    }
+
+    images(): string[] {
+        return this.grid.flat().map((slot) => `/images/sokoban-${slot}.png`);
+    }
+}
 
 const puzzle = `
 WWWW
-WPEW
-WBBW
+WPGW
+WEBW
 WWWW
 `;
 
-const gridImageMap = {
-    b: "/images/sokoban-block.png",
-    e: "/images/sokoban-empty.png",
-    p: "/images/sokoban-player.png",
-    w: "/images/sokoban-wall.png",
-} as const;
-
-const grid: string[][] = puzzle
-    .trim()
-    .split("\n")
-    .map((row) =>
-        row
-            .trim()
-            .toLowerCase()
-            .split("")
-            .map((key) => gridImageMap[key as keyof typeof gridImageMap]),
-    );
+const grid = new GameState(puzzle);
 
 function App() {
-    console.log(grid);
+    const images = useMemo(() => grid.images(), []);
+    const width = grid.w;
+    const height = grid.h;
+
     return (
         <>
             <div
                 style={{
                     display: "grid",
-                    gridTemplateColumns: "repeat(4, 64px [col])",
-                    gridTemplateRows: "repeat(4, 64px [row])",
+                    gridTemplateColumns: `repeat(${width}, ${CELL_SIZE}px [col])`,
+                    gridTemplateRows: `repeat(${height}, ${CELL_SIZE}px [row])`,
                 }}
             >
-                {grid.flat().map((src) => (
-                    <img src={src} style={{ imageRendering: "pixelated", scale: "4" }} />
-                ))}
+                {images.map((src) => [
+                    <img src={src} style={{ imageRendering: "pixelated", scale: "4" }} />,
+                    // <div></div>,
+                ])}
             </div>
         </>
     );
