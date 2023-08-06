f3226b19c81d268c7891e31dea874285e9b1e47d
Noah May <noahmouse2011@gmail.com>
2023-08-06 17:27:31 -0500

    misc-python: Differences between tasks and coroutines

--- misc-python/src/asyncio_.py
+++ misc-python/src/asyncio_.py
@@ -2,13 +2,15 @@ import asyncio
 
 
 async def main():
-    await first()
-    await second()
+    task1 = asyncio.ensure_future(first())
+    task2 = asyncio.ensure_future(second())
+    await task1
+    await task2
 
 
 async def first():
     print("first 1")
-    await asyncio.sleep(0.5)
+    await asyncio.sleep(1)
     print("first 2")
 
 
