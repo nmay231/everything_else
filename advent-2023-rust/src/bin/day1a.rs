674758ea073ca1a96816a3e7818db973eee04d26
Noah May <noahmouse2011@gmail.com>
2023-12-02 15:39:26 -0600

    advent-2023-rust: day1a

--- advent-2023-rust/src/bin/day1a.rs
+++ advent-2023-rust/src/bin/day1a.rs
@@ -2,5 +2,20 @@ use std::fs::read_to_string;
 
 fn main() {
     let text = read_to_string("./assets/day1.txt").unwrap();
-    println!("{}", text.lines().next().unwrap());
+    let mut sum = 0;
+    for line in text.lines() {
+        let mut first = None;
+        let mut last = 0;
+        for char in line.chars() {
+            if let Some(digit) = char.to_digit(10) {
+                if first == None {
+                    first = Some(digit);
+                }
+                last = digit;
+            }
+        }
+
+        sum += (first.unwrap()) * 10 + last;
+    }
+    println!("The sum is: {}", sum);
 }
