d7a26324355f45c2e58ac84e7f33d624dae60315
Noah May <noahmouse2011@gmail.com>
2023-12-14 15:18:47 -0600

    advent-2023-rust: day12a

--- advent-2023-rust/src/bin/day12.rs
+++ advent-2023-rust/src/bin/day12.rs
@@ -1,181 +1,139 @@
-use std::marker::PhantomData;
-use std::ops::{Bound, Range, RangeBounds};
-use std::slice::SliceIndex;
-
 type Output = usize;
 
+const DEBUGGING: bool = false;
+
+macro_rules! debug_println {
+    () => {
+        if DEBUGGING {
+            print!("\n")
+        }
+    };
+    ($($arg:tt)*) => {{
+        if DEBUGGING {
+            println!("{}", format!($($arg)*))
+        }
+    }};
+}
+
 pub fn count_spring_row_configs(springs: &str, contiguous_broken_count: Vec<usize>) -> usize {
-    0
+    let springs_start = springs.find(|char| char != '.').unwrap();
+    let springs_end = springs.rfind(|char| char != '.').unwrap();
+    let springs = &springs[springs_start..=springs_end];
+
+    let maybe_broken = springs
+        .split('.')
+        .filter_map(|str| if str.len() > 0 { Some(str.len()) } else { None })
+        .collect::<Vec<_>>();
+
+    count_spring_row_configs_(springs, &maybe_broken, &contiguous_broken_count)
 }
 
-enum Which<T> {
-    Broken(T),
-    Working(T),
-    Unknown(T),
-}
-
-impl<T> Which<T> {
-    fn unwrap(&self) -> &T {
-        match self {
-            Which::Broken(n) => n,
-            Which::Working(n) => n,
-            Which::Unknown(n) => n,
-        }
-    }
-}
-
-trait IndexList<'a, T, I, R: RangeBounds<usize>>: std::ops::Index<I, Output = T> {
-    fn iter_range(&'a self, range: R) -> IterIndexes<'a, T, I, R, dyn IndexList<'a, T, I, R>>;
-    fn unwrap_index(&self, index: usize) -> I;
-}
-
-struct IterIndexes<'a, T, I, R: RangeBounds<usize>, IL: IndexList<'a, T, I, R> + ?Sized> {
-    refer: &'a IL,
-    range: R,
-    index: usize,
-    value_type: PhantomData<T>,
-    index_type: PhantomData<I>,
-}
-
-impl<'a, T, I, R: RangeBounds<usize>, IL: IndexList<'a, T, I, R> + ?Sized>
-    IterIndexes<'a, T, I, R, IL>
-{
-    fn new(refer: &'a IL, range: R) -> Self {
-        Self {
-            refer,
-            range,
-            index: 0,
-            value_type: PhantomData,
-            index_type: PhantomData,
-        }
-    }
-}
-
-impl<'a, T, I, R: RangeBounds<usize>, IL: IndexList<'a, T, I, R> + ?Sized> Iterator
-    for IterIndexes<'a, T, I, R, IL>
-{
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let start = match self.range.start_bound() {
-            Bound::Unbounded => usize::MIN,
-            Bound::Included(s) => *s,
-            Bound::Excluded(s) => s.saturating_add(1),
-        };
-        let end = match self.range.end_bound() {
-            Bound::Unbounded => usize::MAX,
-            Bound::Included(&e) => e,
-            Bound::Excluded(e) => e.saturating_sub(1),
-        };
-
-        // TODO: Don't construct a range each time I need to get the next item
-        todo!();
-        // let index = self.index;
-        // self.index += 1;
-        // return (start..end).nth(index).map(|i| self.refer.unwrap_index(i));
-    }
-}
-
-impl<'a, R: RangeBounds<usize>> IndexList<'a, usize, usize, R> for [usize]
-where
-    Self: Sized,
-{
-    fn iter_range(
-        &'a self,
-        range: R,
-    ) -> IterIndexes<'a, usize, usize, R, dyn IndexList<'a, usize, usize, R>> {
-        return IterIndexes::new(*Box::new(self).as_ref(), range);
-    }
-
-    fn unwrap_index(&self, index: usize) -> usize {
-        return self[index];
-    }
-}
-
-/// Returns the number of configurations possible given a set of runs of `Which`
-/// # Arguments
-///
-/// * `runs`
-/// * `working_then_broken` - The alternating length of runs of working and broken or unknown, starting with the number of working.
-///                           Any amount of working springs at the end are irrelevant and are not passed to this function.
-fn _count_spring_row_configs(
+fn count_spring_row_configs_(
     springs: &str,
-    runs: &[Which<usize>],
     maybe_broken: &[usize],
     known_broken: &[usize],
 ) -> usize {
+    debug_println!("{}; {:?}, {:?}", springs, maybe_broken, known_broken);
+
     if known_broken.len() == 0 {
-        // There is one way to satisfy no requirements
-        return 1;
-    } else if maybe_broken.len() == 0 || maybe_broken.iter().sum() < known_broken.iter().sum() {
-        // There must be a configuration where the known broken can fit into possible slots
+        if springs.find('#') == None {
+            debug_println!("There is one way to satisfy no requirements");
+            return 1;
+        } else {
+            debug_println!("Required broken still left over. Can't satisfy requirements");
+            return 0;
+        }
+    } else if maybe_broken.len() == 0
+        || maybe_broken.iter().sum::<usize>() < known_broken.iter().sum()
+    {
+        debug_println!("No fit: There must be a configuration where the known broken can fit into possible slots");
         return 0;
     }
 
-    let next_broken_block = runs
-        .iter()
+    let next_broken_slot = springs
+        .chars()
         .enumerate()
-        .find_map(|(i, run)| match run {
-            Which::Working(_) => Some(i + 1), // The index after the first working group
-            Which::Broken(_) | Which::Unknown(_) => None,
-        })
-        .or(Some(runs.len()))
+        .skip_while(|(_, char)| *char != '.')
+        .find_map(|(i, char)| if char != '.' { Some(i) } else { None })
+        .or(Some(springs.len()))
         .unwrap();
-    let next_broken_start = *runs[next_broken_block].unwrap();
+
+    let broken_required_start = springs.find('#').or(Some(springs.len())).unwrap();
 
     if known_broken[0] > maybe_broken[0] {
-        // The slot is too small for this block of broken springs. Skip it.
-        return _count_spring_row_configs(
-            &springs[next_broken_start..],
-            &runs[next_broken_block..],
-            maybe_broken,
+        if broken_required_start < next_broken_slot {
+            debug_println!("This slot is too small for current block, but the block cannot be satisfied. Return 0");
+            return 0;
+        }
+        debug_println!("REENTRY: The slot is too small for this block of broken springs. Skipping");
+        return count_spring_row_configs_(
+            &springs[next_broken_slot..],
+            &maybe_broken[1..],
             known_broken,
         );
-    // } else if known_broken[0] == maybe_broken[0] {
-    //     // This block doesn't affect the total count. TODO: NOT TRUE, it could also start on the next block...
-    //     return _count_spring_row_configs(
-    //         &springs[next_broken_start..],
-    //         &runs[next_broken_block..],
-    //         &maybe_broken[1..],
-    //         &known_broken[1..],
-    //     );
     } else {
-        let if_skipping_this_block = _count_spring_row_configs(
-            &springs[next_broken_start..],
-            &runs[next_broken_block..],
-            maybe_broken,
-            known_broken,
-        );
+        let if_skipping_this_block = if broken_required_start < next_broken_slot {
+            debug_println!("Cannot skip first block");
+            0
+        } else {
+            debug_println!("REENTRY: Check if we can skip the first block");
+            count_spring_row_configs_(
+                &springs[next_broken_slot..],
+                &maybe_broken[1..],
+                known_broken,
+            )
+        };
+        debug_println!("count if can skip first block: {}", if_skipping_this_block);
 
         return if_skipping_this_block
-            + (0..=(maybe_broken[0] - known_broken[0]))
-                .map(|offset| {
-                    let end = known_broken[0] + offset;
+            + (0..=std::cmp::min(maybe_broken[0] - known_broken[0], broken_required_start))
+                .map(|start| {
+                    let end = start + known_broken[0];
+                    if (start == 0 || springs.chars().nth(start - 1) != Some('#'))
+                        && springs.chars().nth(end) != Some('#')
+                    {
+                        let springs = if maybe_broken[0] - end > 1 {
+                            &springs[end + 1..]
+                        } else if end == springs.len() {
+                            ""
+                        } else {
+                            &springs[next_broken_slot..]
+                        };
+
+                        let tmp;
+                        let maybe_broken = if maybe_broken[0] - end > 1 {
+                            tmp = [&[maybe_broken[0] - end - 1], &maybe_broken[1..]].concat();
+                            &tmp[..]
+                        } else {
+                            &maybe_broken[1..]
+                        };
+                        debug_println!("REENTRY: Checking offset into first: {}", start);
+                        return count_spring_row_configs_(
+                            springs,
+                            &maybe_broken,
+                            &known_broken[1..],
+                        );
+                    } else {
+                        return 0;
+                    }
                 })
-                .sum();
-        // for offset in  {
-
-        // }
-
-        let mut run_index;
-        let mut broken = known_broken[0];
-        for (run_index_, run) in runs.iter().enumerate() {
-            let run = run.unwrap();
-            if *run < broken {
-                broken -= run;
-            } else {
-                run_index = run_index_;
-                break;
-            }
-        }
-        // let index_to_runs_indexes = (index into runs, index into that run of springs);
-
-        return (known_broken[0]..maybe_broken[0]).map(|end| {}).sum();
+                .sum::<usize>();
     }
 }
 
 fn part1(text: &str) -> Output {
-    0
+    return text
+        .lines()
+        .map(|line| {
+            let space = line.find(' ').unwrap();
+            let springs = &line[..space];
+            let contiguous_broken_count = line[space + 1..]
+                .split(',')
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            count_spring_row_configs(&springs, contiguous_broken_count)
+        })
+        .sum();
 }
 
 fn part2(_text: &str) -> Output {
@@ -198,12 +156,20 @@ mod test {
     use crate::count_spring_row_configs;
 
     #[rstest]
-    #[case("???.###", vec![1,1,3], 1)]
-    #[case(".??..??...?##.", vec![1,1,3], 4)]
+    #[case(".###.##.#...", vec![3, 2, 1], 1)]
+    #[case("###..##...#", vec![3, 2, 1], 1)]
+    #[case("???.###", vec![1, 1, 3], 1)]
+    #[case(".??..??...?##.", vec![1, 1, 3], 4)]
     #[case("?#?#?#?#?#?#?#?", vec![1, 3, 1, 6], 1)]
     #[case("????.#...#...", vec![4, 1, 1], 1)]
-    #[case("????.######..#####.", vec![1,6,5], 4)]
-    #[case("?###????????", vec![3,2,1], 10)]
+    #[case("?###????????", vec![3, 2, 1], 10)]
+    #[case("????.######..#####.", vec![1, 6, 5], 4)]
+    #[case("???????", vec![3], 5)]
+    #[case("??????????", vec![1, 1], 8*9/2)]
+    #[case("#.#", vec![1], 0)]
+    #[case("#.##", vec![2], 0)]
+    #[case("?.#.#.?.#.#.?", vec![1, 1, 1], 0)]
+    #[case("?.#.#.?.#.#.?", vec![1, 1, 1, 1], 1)]
     fn test_count_spring_row_configs(
         #[case] springs: &str,
         #[case] broken: Vec<usize>,
