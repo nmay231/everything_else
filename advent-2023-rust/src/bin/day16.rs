d4a5fd8d32593e144ff9801f1674a2182eb69c03
Noah May <noahmouse2011@gmail.com>
2023-12-20 22:33:59 -0600

    advent-2023-rust: day16b

--- advent-2023-rust/src/bin/day16.rs
+++ advent-2023-rust/src/bin/day16.rs
@@ -4,9 +4,9 @@ use advent_2023_rust::{Direc, UsizePoint};
 
 type Output = usize;
 
-fn beeeeaaaammm(grid: &[char], grid_size: &UsizePoint) -> usize {
+fn beeeeaaaammm(grid: &[char], grid_size: &UsizePoint, start: (UsizePoint, Direc)) -> usize {
     // Vec<(CurrentPoint, FromDirection)>
-    let mut beams = vec![(UsizePoint(0, 0), Direc::East)];
+    let mut beams = vec![start];
     let mut visited = HashSet::new();
     while let Some((mut point, mut direc)) = beams.pop() {
         loop {
@@ -62,11 +62,29 @@ fn beeeeaaaammm(grid: &[char], grid_size: &UsizePoint) -> usize {
 fn part1(text: &str) -> Output {
     let grid_size = UsizePoint(text.lines().count(), text.lines().next().unwrap().len());
     let grid = text.trim().replace('\n', "").chars().collect::<Vec<_>>();
-    return beeeeaaaammm(&grid, &grid_size);
+    return beeeeaaaammm(&grid, &grid_size, (UsizePoint(0, 0), Direc::East));
 }
 
-fn part2(_text: &str) -> Output {
-    0
+fn part2(text: &str) -> Output {
+    let grid_size = UsizePoint(text.lines().count(), text.lines().next().unwrap().len());
+    let grid = text.trim().replace('\n', "").chars().collect::<Vec<_>>();
+
+    let mut max = 0;
+    for col in 0..grid_size.1 {
+        for (row, direc) in [(0, Direc::South), (grid_size.0 - 1, Direc::North)] {
+            let amount = beeeeaaaammm(&grid, &grid_size, (UsizePoint(row, col), direc));
+            assert!(amount > 1); // Sanity check
+            max = std::cmp::max(max, amount);
+        }
+    }
+    for row in 0..grid_size.0 {
+        for (col, direc) in [(0, Direc::East), (grid_size.1 - 1, Direc::West)] {
+            let amount = beeeeaaaammm(&grid, &grid_size, (UsizePoint(row, col), direc));
+            assert!(amount > 1); // Sanity check
+            max = std::cmp::max(max, amount);
+        }
+    }
+    max
 }
 
 fn main() -> std::io::Result<()> {
@@ -80,30 +98,49 @@ fn main() -> std::io::Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use advent_2023_rust::UsizePoint;
+    use advent_2023_rust::{Direc, UsizePoint};
     use indoc::indoc;
 
-    use crate::beeeeaaaammm;
+    use crate::{beeeeaaaammm, part2};
+
+    const INPUT: &str = indoc! {r#"
+    .|...\....
+    |.-.\.....
+    .....|-...
+    ........|.
+    ..........
+    .........\
+    ..../.\\..
+    .-.-/..|..
+    .|....-|.\
+    ..//.|...."#};
 
     #[test]
-    fn given_example() {
-        let input = indoc! {r#"
-        .|...\....
-        |.-.\.....
-        .....|-...
-        ........|.
-        ..........
-        .........\
-        ..../.\\..
-        .-.-/..|..
-        .|....-|.\
-        ..//.|...."#};
+    fn first_example() {
         assert_eq!(
             beeeeaaaammm(
-                &input.replace('\n', "").chars().collect::<Vec<_>>(),
-                &UsizePoint(10, 10)
+                &INPUT.replace('\n', "").chars().collect::<Vec<_>>(),
+                &UsizePoint(10, 10),
+                (UsizePoint(0, 0), Direc::East),
             ),
-            46
+            46,
         );
+    }
+
+    #[test]
+    fn best_example() {
+        assert_eq!(
+            beeeeaaaammm(
+                &INPUT.replace('\n', "").chars().collect::<Vec<_>>(),
+                &UsizePoint(10, 10),
+                (UsizePoint(0, 3), Direc::South),
+            ),
+            51,
+        );
+    }
+
+    #[test]
+    fn can_find_best_example() {
+        assert_eq!(part2(INPUT), 51);
     }
 }
