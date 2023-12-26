44cc4d8952dcb1472a2559671f8d0276fd19cff2
Noah May <noahmouse2011@gmail.com>
2023-12-26 01:20:36 -0600

    advent-2023-rust: day23a

---
+++ advent-2023-rust/src/bin/day23.rs
@@ -0,0 +1,85 @@
+use advent_2023_rust::{Direc, UsizePoint};
+use itertools::Itertools;
+
+type Output = usize;
+
+fn part1(text: &str) -> Output {
+    let grid_size = &UsizePoint(text.lines().count(), text.find('\n').unwrap());
+    let grid = text.trim().replace('\n', "").chars().collect_vec();
+
+    let destination = UsizePoint(grid_size.0 - 1, grid_size.1 - 2);
+    assert_eq!(grid[destination.as_index(grid_size)], '.');
+    assert_eq!(destination.next_point(&Direc::South, grid_size), None);
+
+    let mut paths = vec![(UsizePoint(0, 1), Direc::North, vec![])];
+    let mut max_distance = 0;
+    while let Some((mut point, mut from_direc, mut visited)) = paths.pop() {
+        loop {
+            let next = Direc::POWERS_OF_I
+                .iter()
+                .filter_map(|direc| {
+                    if direc == &from_direc {
+                        None
+                    } else {
+                        let next_point = point.next_point(direc, grid_size)?;
+                        let char = grid[next_point.as_index(grid_size)];
+                        match (char, direc) {
+                            ('.', _)
+                            | ('>', Direc::East)
+                            | ('<', Direc::West)
+                            | ('v', Direc::South)
+                            | ('^', Direc::North) => Some((next_point, direc.rotate(2))),
+                            ('#' | '>' | '<' | 'v' | '^', _) => None,
+                            _ => {
+                                unreachable!("Unexpected character at {:?}: '{}'", next_point, char)
+                            }
+                        }
+                    }
+                })
+                .collect_vec();
+
+            if next.len() == 0 {
+                if point == destination {
+                    max_distance = std::cmp::max(max_distance, visited.len());
+                }
+                break; // Dead-end
+            } else {
+                if next.iter().skip(1).any(|(point, _)| point == &destination) {
+                    println!("{:?}", (visited.len(), &visited));
+                }
+                visited.push(point);
+                (point, from_direc) = next[0];
+
+                if next.len() > 1 {
+                    paths.extend(
+                        next.into_iter()
+                            .skip(1)
+                            .map(|(start, from_direc)| (start, from_direc, visited.clone())),
+                    )
+                }
+            }
+        }
+    }
+    max_distance
+}
+
+fn part2(_text: &str) -> Output {
+    0
+}
+
+fn main() -> std::io::Result<()> {
+    let text = std::fs::read_to_string("./assets/day23.txt")?;
+
+    println!("part 1 result = {:?}", part1(&text));
+    println!("part 2 result = {:?}", part2(&text));
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn sample() {
+        assert!(true);
+    }
+}
