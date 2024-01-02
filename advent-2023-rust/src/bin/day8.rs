89335f2d868ade61b2faf830ec2bcb03446ee093
Noah May <noahmouse2011@gmail.com>
2024-01-02 01:59:33 -0600

    advent-2023-rust: day8b (works; mostly correct)

--- advent-2023-rust/src/bin/day8.rs
+++ advent-2023-rust/src/bin/day8.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 use std::ops::Deref;
 
+use itertools::Itertools;
+use num_integer::{ExtendedGcd, Integer};
+
 type Result = usize;
 
 fn part1(text: &str) -> Result {
@@ -30,23 +33,6 @@ fn part1(text: &str) -> Result {
     return 0;
 }
 
-fn gcd(a: Result, b: Result) -> Result {
-    let mut max = a;
-    let mut min = b;
-    if min > max {
-        (min, max) = (max, min);
-    }
-
-    loop {
-        let res = max % min;
-        if res == 0 {
-            return min;
-        }
-
-        (max, min) = (min, res);
-    }
-}
-
 // Unfortunately, I peeked at the subreddit so the solution was a bit spoiled for me, even though it isn't the right solution given the problem in general.
 // I don't know how to feel about it since I can't solve on my own anymore. I'm not gonna look at the subreddit anymore until I've solved the whole advent, or at least as much as I'm willing to solve.
 // Based on a user's comment in the following thread, I at least know the way I would solve it is to use the Chinese remainder theorem.
@@ -68,45 +54,66 @@ fn part2(text: &str) -> Result {
         map.insert(&line[..3], (&line[7..10], &line[12..15]));
     }
 
-    return map
+    let seeds = map
         .keys()
         .map(Deref::deref)
-        .filter(|pos| pos.ends_with("A"))
-        .map(|start| {
-            let mut pos = start;
-            let mut visited = HashMap::new();
-            visited.insert((0, pos), 0);
+        .filter(|str| str.ends_with('A'))
+        .collect_vec();
 
-            for (steps, (i, left)) in turns.iter().enumerate().cycle().enumerate() {
-                let pair = map.get(pos).unwrap();
-                pos = if *left { pair.0 } else { pair.1 };
+    let mut offset_cycle_pairs = vec![];
+    for mut seed in seeds {
+        let mut visited = vec![(seed, 0)];
+        for (turn_index, turn) in turns.iter().enumerate().cycle() {
+            let branch = map.get(seed).unwrap();
+            seed = if *turn { branch.0 } else { branch.1 };
 
-                if pos.ends_with("Z") {
-                    println!("end pos={pos}");
-                    return 0;
-                }
-                let key = (i + 1, pos);
-                if visited.contains_key(&key) {
-                    println!("{visited:?}; {pos}");
-                    panic!();
-                }
-                visited.insert(key, steps + 1);
-
-                if pos == start {
-                    // TODO: This is not always true, I need the visited array, but also,
-                    return steps + 1;
-                }
+            if let Some(index) = visited.iter().position(|x| x == &(seed, turn_index)) {
+                assert_eq!((visited.len() - index) % turns.len(), 0);
+                offset_cycle_pairs.push((
+                    index as isize,
+                    ((visited.len() - index) / turns.len()) as isize,
+                ));
+                break;
             }
-            return 1;
-        })
-        .map(|steps| {
-            // println!("{steps}");
-            steps
-        })
-        .fold(1 as usize, |acc, steps| steps / gcd(acc, steps) * acc);
-    // .fold(1 as usize, |acc, (modulus, offset)| {
-    //     steps / gcd(acc, steps) * acc
-    // });
+            visited.push((seed, turn_index));
+        }
+    }
+    // let offset_cycle_pairs = vec![
+    //     (3, (20803 / turns.len()) as isize),
+    //     (2, (13771 / turns.len()) as isize),
+    //     (6, (17287 / turns.len()) as isize),
+    //     (2, (19631 / turns.len()) as isize),
+    //     (3, (23147 / turns.len()) as isize),
+    //     (3, (17873 / turns.len()) as isize),
+    // ];
+
+    // TODO: Technically incorrect answer (in general, that is).
+    let wrong_way = turns.len() as isize
+        * offset_cycle_pairs
+            .iter()
+            .fold(1, |acc, (_offset, cycle_length)| acc.lcm(cycle_length));
+    println!("The incorrectly correct answer: {}", wrong_way);
+
+    // TODO: However, I can't even solve it CORRECTLY. And I'm so done right now...
+    // https://brilliant.org/wiki/chinese-remainder-theorem/
+    for (a, b) in offset_cycle_pairs.iter().tuple_combinations() {
+        assert_eq!(a.1.gcd(&b.1), 1);
+    }
+
+    let mut x = 0;
+    let cap_n = offset_cycle_pairs.iter().fold(1, |acc, (_, n_i)| acc * n_i);
+    for (a_i, n_i) in offset_cycle_pairs {
+        let y_i = cap_n / n_i;
+        let ExtendedGcd { x: z_i, .. } = y_i.extended_gcd(&n_i);
+        // let z_i = z_i.rem_euclid(n_i);
+        assert_eq!((z_i * y_i).rem_euclid(n_i), 1);
+
+        x += a_i * y_i * z_i;
+        println!("{:?}", (a_i, y_i, z_i, a_i * y_i * z_i));
+    }
+    let x = x.rem_euclid(cap_n);
+    assert!(x >= 0, "{x}");
+    return x as usize * turns.len();
 }
 
 fn main() -> std::io::Result<()> {
