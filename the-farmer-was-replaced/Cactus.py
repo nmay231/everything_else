5e9738cd19e87756a8113589e37daae899ccbaf7
Noah May <noahmouse2011@gmail.com>
2024-09-18 01:23:55 -0500

    the-farmer-was-replaced: WIP fix to cactus
    For some reason I thought I just had to harvest a cactus that was
    locally sorted, not that all cacti had to be sorted
    
    bubble_sort doesn't work yet, but I want to reorganize the project
    structure to make testing things easier

--- the-farmer-was-replaced/Cactus.py
+++ the-farmer-was-replaced/Cactus.py
@@ -8,52 +8,38 @@ def init_tile():
     plant(Entities.Cactus)
 
 
-def neighbor(foward, backward):
-    move(foward)
-    measurement = measure()
-    move(backward)
-    return measurement
+def bubble_sort(forward, backward, get_pos):
+    _bubble_sort(forward, backward, get_pos, 0, get_world_size() - 1)
+
+
+def _bubble_sort(forward, backward, get_pos, start, end):
+    while get_pos() > 0:
+        move(backward)
+
+    while start < end:
+        for _ in range(end - start):
+            if measure() > measure(forward):
+                swap(forward)
+            move(forward)
+
+        end -= 1
+        for _ in range(end - start):
+            if measure() < measure(backward):
+                swap(backward)
+            move(backward)
+
+        start += 1
+
+    return
 
 
 while True:
-    to_pos(0, 0)
-
-    harvested = False
-    prev_col = None
-    for x in range(get_world_size()):
-        column = []
-        for y in range(get_world_size()):
-            current = measure()
-            column.append((True, current))
-            if y > 0:
-                prev_possible, prev_measure = column[y - 1]
-                if prev_measure > current:
-                    column[y - 1] = (False, prev_measure)
-                    column[y] = (False, current)
-                    continue
-            if x > 0:
-                prev_possible, prev_measure = prev_col[y]
-                if prev_measure > current:
-                    column[y] = (False, current)
-                    # No need to update previous column
-                elif prev_possible:
-                    harvest()
-                    harvested = True
-                    break
-            move(North)
+    for _ in range(get_world_size()):
+        bubble_sort(North, South, get_pos_y)
         move(East)
-
-        if harvested:
-            break
-        prev_col = column
-
-    if not harvested:
-        for y in range(get_world_size()):
-            possible, _ = column[y]
-            if possible:
-                to_pos(get_world_size() - 1, y)
-                harvest()
-                break
-
+    for _ in range(get_world_size()):
+        bubble_sort(East, West, get_pos_x)
+        move(North)
+    harvest()
     stock_to(Items.Cactus_Seed, TILE_COUNT)
     for_each(init_tile)
