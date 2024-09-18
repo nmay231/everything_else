227c8f88112cd855a82afff38639cad60c9e9f64
Noah May <noahmouse2011@gmail.com>
2024-09-18 01:28:03 -0500

    the-farmer-was-replaced: Move the save files into a sub directory

---
+++ the-farmer-was-replaced/src/save-directory/Scratch.py
@@ -0,0 +1,42 @@
+TILE_COUNT = get_world_size() ** 2
+
+stock_to(Items.Pumpkin_Seed, TILE_COUNT)
+clear_grid(Grounds.Soil, Entities.Pumpkin)
+
+
+def init_tile():
+    plant(Entities.Pumpkin)
+
+
+while True:
+    restock_empty_tanks()
+
+    to_check = []
+    for _ in range(TILE_COUNT):
+        if get_entity_type() == None or not can_harvest():
+            to_check.append((get_pos_x(), get_pos_y()))
+            if get_entity_type() == None:
+                plant(Entities.Pumpkin)
+            if get_water() < 0.50:
+                use_item(Items.Water_Tank)
+        move(North)
+        if get_pos_y() == 0:
+            move(East)
+
+    while len(to_check) > 0:
+        x, y = to_check.pop(0)
+        to_pos(x, y)
+
+        if get_entity_type() != None and not can_harvest():
+            do_a_flip()
+
+        if can_harvest():
+            continue
+        elif get_entity_type() == None:
+            trade(Items.Pumpkin_Seed)
+            plant(Entities.Pumpkin)
+        to_check.append((x, y))
+    harvest()
+
+    stock_to(Items.Pumpkin_Seed, TILE_COUNT)
+    for_each(init_tile)
