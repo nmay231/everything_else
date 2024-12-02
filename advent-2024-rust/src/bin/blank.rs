01f76776bc908be7d7a95257598ad0ecf4f8d710
Noah May <noahmouse2011@gmail.com>
2024-12-01 22:51:59 -0700

    advent-2024-rust: Most of the time, the output is a single number

--- advent-2024-rust/src/bin/blank.rs
+++ advent-2024-rust/src/bin/blank.rs
@@ -1,8 +1,12 @@
-type Output = ();
+type Output = usize;
 
-fn part1(text: &str) -> Output {}
+fn part1(text: &str) -> Output {
+    0
+}
 
-fn part2(_text: &str) -> Output {}
+fn part2(_text: &str) -> Output {
+    0
+}
 
 fn main() -> std::io::Result<()> {
     let text = std::fs::read_to_string("./assets/day%DAY_NUMBER%.txt")?;
