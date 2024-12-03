8caaf12a9bf6d5a3a502f1e5d192a69a4220a0ce
Noah May <noahmouse2011@gmail.com>
2024-12-02 22:15:19 -0700

    advent-2024-rust: day3a

---
+++ advent-2024-rust/src/bin/day3.rs
@@ -0,0 +1,43 @@
+use std::str::FromStr;
+
+use regex::Regex;
+
+type Output = usize;
+
+fn part1(text: &str) -> Output {
+    // Sometimes a regex IS the right answer!
+    let mul = Regex::from_str(r"mul\(\d+,\d+\)").unwrap();
+    let mut result = 0;
+    for mat in mul.find_iter(text) {
+        let sub = mat.as_str();
+        let sub = &sub[4..].trim_end_matches(')');
+        let (a, b) = sub
+            .split_once(',')
+            .expect("to have two numbers split by comma");
+        let a: usize = a.parse().unwrap();
+        let b: usize = b.parse().unwrap();
+        result += a * b;
+    }
+    result
+}
+
+fn part2(text: &str) -> Output {
+    0
+}
+
+fn main() -> std::io::Result<()> {
+    let text = std::fs::read_to_string("./assets/day3.txt")?;
+
+    println!("part 1 result = {:?}", part1(&text));
+    println!("part 2 result = {:?}", part2(&text));
+
+    Ok(())
+}
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn todo!() {
+//         assert!(true);
+//     }
+// }
