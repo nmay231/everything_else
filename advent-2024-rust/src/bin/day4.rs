bb2c5817fb74b0eeadcfff3d453239d63fbee53b
Noah May <noahmouse2011@gmail.com>
2024-12-09 09:13:39 -0700

    advent-2024-rust: day4b

--- advent-2024-rust/src/bin/day4.rs
+++ advent-2024-rust/src/bin/day4.rs
@@ -34,7 +34,54 @@ fn part1(text: &str) -> Output {
 }
 
 fn part2(text: &str) -> Output {
-    0
+    let grid_size = &UsizePoint(text.lines().count(), text.find('\n').unwrap());
+    let grid = text.replace('\n', "").trim().chars().collect::<Vec<_>>();
+    let mut total = 0;
+
+    'next_char: for (i, c) in grid.iter().enumerate() {
+        if c != &'A' {
+            continue;
+        }
+
+        let corners = [
+            [Direc::East, Direc::North],
+            [Direc::West, Direc::North],
+            [Direc::West, Direc::South],
+            [Direc::East, Direc::South],
+        ];
+
+        let center = UsizePoint::from_index(grid_size, i);
+        for i in 0..=1 {
+            let opposing = [corners[i], corners[(i + 2) % 4]];
+            let points = opposing
+                .iter()
+                .map(|[a, b]| {
+                    center
+                        .clone()
+                        .next_point(a, grid_size)
+                        .and_then(|horizontal| horizontal.next_point(b, grid_size))
+                })
+                .collect::<Option<Vec<_>>>();
+
+            match points {
+                None => continue 'next_char,
+                Some(opposite) => {
+                    if let [a, b] = opposite[0..2] {
+                        let a = grid[a.as_index(grid_size)];
+                        let b = grid[b.as_index(grid_size)];
+                        if [a, b] != ['M', 'S'] && [b, a] != ['M', 'S'] {
+                            continue 'next_char;
+                        }
+                    } else {
+                        panic!("Only two items should be possible.")
+                    }
+                }
+            }
+        }
+        total += 1;
+    }
+
+    total
 }
 
 fn main() -> std::io::Result<()> {
@@ -50,22 +97,28 @@ fn main() -> std::io::Result<()> {
 mod tests {
     use indoc::indoc;
 
-    use crate::part1;
+    use crate::{part1, part2};
+
+    const TEXT: &str = indoc! {"
+        MMMSXXMASM
+        MSAMXMSMSA
+        AMXSXMAAMM
+        MSAMASMSMX
+        XMASAMXAMM
+        XXAMMXXAMA
+        SMSMSASXSS
+        SAXAMASAAA
+        MAMMMXMMMM
+        MXMXAXMASX
+    "};
 
     #[test]
     fn test_part1_example() {
-        let text = indoc! {"
-            MMMSXXMASM
-            MSAMXMSMSA
-            AMXSXMAAMM
-            MSAMASMSMX
-            XMASAMXAMM
-            XXAMMXXAMA
-            SMSMSASXSS
-            SAXAMASAAA
-            MAMMMXMMMM
-            MXMXAXMASX
-        "};
-        assert_eq!(part1(text), 18);
+        assert_eq!(part1(TEXT), 18);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        assert_eq!(part2(TEXT), 9);
     }
 }
