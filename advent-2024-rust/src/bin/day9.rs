3fecf09699a840dfa98cc60f8c23dce24335fe6f
Noah May <noahmouse2011@gmail.com>
2024-12-16 17:54:07 -0700

    advent-2024-rust: day9b

--- advent-2024-rust/src/bin/day9.rs
+++ advent-2024-rust/src/bin/day9.rs
@@ -1,5 +1,7 @@
 use std::collections::VecDeque;
 
+use itertools::repeat_n;
+
 type Output = usize;
 
 fn _debug_file_system(text: &str) {
@@ -11,7 +13,7 @@ fn _debug_file_system(text: &str) {
                 format!("{}", i / 2).repeat(c.to_digit(10).unwrap() as usize)
             );
         } else {
-            print!("{}", "0".repeat(c.to_digit(10).unwrap() as usize));
+            print!("{}", ".".repeat(c.to_digit(10).unwrap() as usize));
         }
     }
     println!();
@@ -99,8 +101,81 @@ fn part1(text: &str) -> Output {
     return total;
 }
 
+/// Unfortunately, I don't see a way to solve part2 without physically
+/// allocating memory for each block. I guess I could keep track of the files
+/// storing them as ranges, but that is more trouble than it is worth.
 fn part2(text: &str) -> Output {
-    0
+    let text = text.trim();
+    // Technically could do 9 * text.len(), but it doesn't matter much anyways
+    let mut blocks = Vec::with_capacity(text.len());
+
+    // We skip the first file so that 0 can mean empty blocks
+    for (i, c) in text.char_indices().skip(1) {
+        let id_or_empty = if i & 1 == 0 { i / 2 } else { 0 };
+        let n = c.to_digit(10).unwrap() as usize;
+        blocks.extend(repeat_n(id_or_empty, n));
+    }
+
+    let mut min_left_index_by_len = [0; 10];
+
+    let mut cur_id = text.len() / 2;
+    let mut right_index = blocks.len() - 1;
+    while cur_id > 0 {
+        // (after the first loop) find the last block of the current file
+        while right_index > 0 && blocks[right_index] != cur_id {
+            right_index -= 1;
+        }
+
+        // Determine the length of the file
+        let mut len = 0;
+        while right_index > 0 && blocks[right_index] == cur_id {
+            right_index -= 1;
+            len += 1;
+        }
+        right_index += 1; // Now indexes the first block of the file
+
+        let mut left_index = min_left_index_by_len[len];
+        if left_index >= right_index {
+            // The earliest gap with the same length as the file is to the right
+            // of the file
+            cur_id -= 1;
+            continue;
+        }
+
+        // Scan from the left finding the earliest gap of the same length
+        let mut gap = 0;
+        while gap < len && left_index < right_index {
+            if blocks[left_index] == 0 {
+                gap += 1;
+            } else {
+                gap = 0;
+            }
+            left_index += 1;
+        }
+
+        if gap == len {
+            // Swap the file with the gap
+            for (a, b) in (left_index - len..left_index).zip(right_index..right_index + len) {
+                blocks.swap(a, b);
+            }
+        }
+
+        // Whether a gap was found or not, update the earliest possible index
+        // for a gap of that size or greater
+        for i in len..10 {
+            min_left_index_by_len[i] = left_index;
+        }
+
+        cur_id -= 1;
+    }
+
+    let first_file_len = text.chars().next().unwrap().to_digit(10).unwrap() as usize;
+    let mut total = 0;
+    for (i, file_id) in blocks.iter().enumerate() {
+        total += file_id * (i + first_file_len);
+    }
+
+    total
 }
 
 fn main() -> std::io::Result<()> {
@@ -114,7 +189,7 @@ fn main() -> std::io::Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{_debug_file_system, part1};
+    use crate::{_debug_file_system, part1, part2};
     use rstest::rstest;
 
     #[rstest]
@@ -132,5 +207,20 @@ mod tests {
         _debug_file_system(text);
 
         assert_eq!(part1(text), result);
+    }
+
+    #[rstest]
+    #[case::packed1("009", 0+1+2+3+4+5+6+7+8)]
+    // It's ambiguous if this is supposed to be allowed, but it didn't seem to
+    // matter anyways...
+    // #[case::packed2_5("059", 0+1+2+3+4+5+6+7+8)]
+    #[case::packed2("099", 0+1+2+3+4+5+6+7+8)]
+    #[case::packed3("54321", 2*5 + 1*(6+7+8))]
+    #[case::given_example1("12345", 3+4+5 + 2*(10+11+12+13+14))]
+    #[case::given_example2("2333133121414131402", 2858)]
+    fn part2_cases(#[case] text: &str, #[case] result: usize) {
+        _debug_file_system(text);
+
+        assert_eq!(part2(text), result);
     }
 }
