ecaa3df84b8857865e72ee1b9733308518fc003a
Noah May <noahmouse2011@gmail.com>
2024-12-16 19:09:45 -0700

    advent-2024-rust: day10b (I actually solved it like this first)

--- advent-2024-rust/src/bin/day10.rs
+++ advent-2024-rust/src/bin/day10.rs
@@ -45,8 +45,42 @@ fn part1(text: &str) -> Output {
     total
 }
 
-fn part2(_text: &str) -> Output {
-    0
+fn part2(text: &str) -> Output {
+    let text = text.trim();
+    let grid_size = &UsizePoint(text.find('\n').unwrap(), text.lines().count());
+    let grid = text.chars().filter(|c| c != &'\n').collect::<Vec<_>>();
+    let digits = "0123456789".chars().collect::<Vec<_>>();
+
+    let mut total = 0;
+    for (index, character) in grid.iter().enumerate() {
+        if character != &'0' {
+            continue;
+        }
+
+        let pos = UsizePoint::from_index(grid_size, index);
+        let mut frontier = vec![(pos, 1_usize)];
+
+        while let Some((pos, next_digit)) = frontier.pop() {
+            for direc in Direc::POWERS_OF_I {
+                let pos = match pos.next_point(&direc, grid_size) {
+                    Some(pos) => pos,
+                    None => continue,
+                };
+
+                if grid[pos.as_index(grid_size)] != digits[next_digit] {
+                    continue;
+                }
+
+                if next_digit == 9 {
+                    total += 1;
+                    continue;
+                }
+                frontier.push((pos, next_digit + 1));
+            }
+        }
+    }
+
+    total
 }
 
 fn main() -> std::io::Result<()> {
@@ -60,7 +94,7 @@ fn main() -> std::io::Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use crate::part1;
+    use crate::{part1, part2};
     use indoc::indoc;
 
     const TEXT1: &str = indoc! {"
@@ -77,5 +111,10 @@ mod tests {
     #[test]
     fn part1_given_example() {
         assert_eq!(part1(TEXT1), 36);
+    }
+
+    #[test]
+    fn part2_given_example() {
+        assert_eq!(part2(TEXT1), 81);
     }
 }
