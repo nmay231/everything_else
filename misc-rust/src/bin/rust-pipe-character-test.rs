9bfae437767406cc72a088eb90734fa95b5eb695
Noah May <noahmouse2011@gmail.com>
2025-02-12 19:50:15 -0600

    misc-rust: rust-pipe-character-test.rs

--- misc-rust/src/bin/rust-pipe-character-test.rs
+++ misc-rust/src/bin/rust-pipe-character-test.rs
@@ -8,6 +8,9 @@ fn f(x: i32) -> bool {
     false
 }
 
+// I don't know why this was novel to me when I wrote this. I guess I was just
+// surprised that booleans accept binary operators. In any case, this is only a
+// recent commit because I want to delete the other top-level directory.
 fn main() {
     println!("1 {}", t(1) | f(1));
     println!("2 {}", t(2) | t(2));
