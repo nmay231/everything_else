29fd0f06bc35fd0126421a694e7ed29c9ce03c69
Noah May <noahmouse2011@gmail.com>
2025-02-14 05:23:48 -0600

    calculator-parser: WIP

--- calculator-parser/src/main.rs
+++ calculator-parser/src/main.rs
@@ -1,3 +1,5 @@
+use std::f128::consts::E;
+
 #[derive(Debug)]
 pub(crate) enum Token<'parse> {
     Number(&'parse str),
@@ -75,11 +77,136 @@ impl<'parse> TokenList<'parse> {
         Self { text, tokens }
     }
 
-    pub fn text(&self) -> &str {
+    pub(crate) fn text(&self) -> &str {
         &self.text
+    }
+
+    pub(crate) fn consume(self) -> Vec<Token<'parse>> {
+        return self.tokens;
+    }
+}
+
+#[derive(Debug)]
+enum Expression<'parse> {
+    U32(u32),
+    Add(&'parse Expression<'parse>, &'parse Expression<'parse>),
+    Subtract(&'parse Expression<'parse>, &'parse Expression<'parse>),
+    Multiply(&'parse Expression<'parse>, &'parse Expression<'parse>),
+    Divide(&'parse Expression<'parse>, &'parse Expression<'parse>),
+    Power(&'parse Expression<'parse>, &'parse Expression<'parse>),
+}
+
+#[derive(Debug, Default)]
+struct PartialExpression<'parse> {
+    operator: Option<Token<'parse>>,
+    lhs: Option<&'parse Expression<'parse>>,
+    rhs: Option<&'parse Expression<'parse>>,
+}
+
+#[derive(Debug)]
+struct ParsedTree<'parse>(Expression<'parse>);
+
+#[derive(Debug)]
+pub(crate) struct Parsed<'parse> {
+    text: &'parse str,
+    expressions: Vec<Expression<'parse>>,
+    tree: ParsedTree<'parse>,
+}
+
+#[derive(Debug)]
+enum ParserState<'parse> {
+    AfterOperator {
+        operator: Token<'parse>,
+        lhs: Option<&'parse Expression<'parse>>,
+    },
+    AfterNumber {},
+}
+
+struct Parsing<'parse> {
+    input_tokens: Vec<Token<'parse>>,
+    stack: Vec<Token<'parse>>,
+    partial_thingy: Vec<PartialParsedThingy<'parse>>,
+    expressions: Vec<Expression<'parse>>,
+}
+
+enum NakedOperator {
+    Plus,
+    Minus,
+    Times,
+    Divide,
+    Power,
+}
+
+enum PartialParsedThingy<'parse> {
+    Number(u32),
+    NumberOperator(&'parse Expression<'parse>, NakedOperator),
+    Triple(&'parse Expression<'parse>, NakedOperator, &'parse PartialParsedThingy<'parse>),
+}
+
+impl<'parse> Parsing<'parse> {
+    fn expect_number_or_lparen(&mut self) -> anyhow::Result<()> {
+        while let Some(Token::LParen) = self.input_tokens.pop() {
+            self.stack.push(Token::LParen);
+            self.expect_number_or_lparen()?;
+        }
+        match self.input_tokens.pop() {
+            Some(Token::Number(text)) => Ok(text),
+            Some(token) => Err(anyhow::anyhow!("Expected number, got {:?}", token)),
+            None => Err(anyhow::anyhow!("Expected something, got nothing")),
+        }
+    }
+}
+
+impl<'parse> Parsed<'parse> {
+    fn new(tokens: TokenList<'parse>) -> anyhow::Result<Self> {
+        let mut stack = vec![];
+        let mut expressions = vec![];
+        let tokens = tokens.consume().into_iter().rev().collect::<Vec<_>>();
+
+        while let Some(Token::LParen) = tokens.pop() {
+            stack.push(Token::LParen);
+        }
+
+        match tokens.pop() {
+            Some(token @ Token::Number(_)) => {
+                stack.push(token);
+            }
+            _ => return Err(anyhow::anyhow!("Expected at least one number")),
+        }
+
+        while let Some(token) = tokens.pop() {
+            match token {
+                Token::Number(_) => return Err(anyhow::anyhow!("Number after number")),
+                token@ (Token::Times | Token::Divide | Token::Power) => {
+                    ;
+            }
+        }
+    }
+
+    fn new_dont_know(tokens: TokenList<'parse>) -> anyhow::Result<Self> {
+        let mut expressions = vec![];
+        let tokens = tokens.consume();
+        let mut root = PartialExpression::default();
+        for token in &tokens {
+            let x = match token {
+                // TODO: Handle numbers larger than u32
+                Token::Number(text) => Some(Expression::U32(text.parse()?)),
+                Token::Plus => todo!(),
+                Token::Minus => todo!(),
+                Token::Times => todo!(),
+                Token::Divide => todo!(),
+                Token::Power => todo!(),
+                Token::LParen => todo!(),
+                Token::RParen => todo!(),
+                Token::Unknown(_) => panic!("Unknown token"),
+            };
+        }
+        let mut tree = ParsedTree(Expression::Number("0"));
     }
 }
 
 fn main() {
-    println!("{:?}", TokenList::new("1 + 2 * 3 / unknown"));
+    let tokens = TokenList::new("1 + 2 * 3 / unknown");
+    let parsed = Parsed::new(tokens);
+    println!("{:?}", parsed);
 }
