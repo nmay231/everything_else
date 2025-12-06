b6e5b30ff46564dd6e95b3c327e3cea3183a770c
Noah May <noahmouse2011@gmail.com>
2025-12-06 14:48:18 -0700

    scripting: sparse-checkout.sh - Random note

--- scripting/sparse-checkout.sh
+++ scripting/sparse-checkout.sh
@@ -1,1 +1,2 @@
 # TODO: https://askubuntu.com/questions/460885/how-to-clone-only-some-directories-from-a-git-repository
+# https://github.blog/open-source/git/get-up-to-speed-with-partial-clone-and-shallow-clone/
