c8ba804471bf10bde1e2c28cd6cdbb024439613c
Noah May <noahmouse2011@gmail.com>
2025-12-07 19:35:40 -0700

    advent-2025-rust: day4b

--- advent-2025-rust/src/bin/day4.rs
+++ advent-2025-rust/src/bin/day4.rs
@@ -37,8 +37,45 @@ fn part1(text: &str) -> usize {
     total
 }
 
-fn part2(_text: &str) -> usize {
-    0
+fn part2(text: &str) -> usize {
+    let grid_size = &Point::<usize>::new_xy(text.find('\n').unwrap(), text.lines().count());
+    let mut grid = text.chars().filter(|c| c != &'\n').collect::<Vec<_>>();
+
+    let mut total = 0;
+    let mut unchecked = (0..grid.len()).collect::<Vec<_>>();
+
+    while let Some(index) = unchecked.pop() {
+        if grid[index] != '@' {
+            continue;
+        }
+
+        let point = Point::<usize>::from_index(grid_size, index);
+        let mut neighbors = Vec::new();
+
+        'neighbors: for path in Direc::EIGHT_WAYS {
+            let mut point = point;
+
+            for direc in path {
+                point = match point.next_point(direc, grid_size) {
+                    Some(p) => p,
+                    None => continue 'neighbors,
+                };
+            }
+
+            let neigh = point.as_index(grid_size);
+            if grid[neigh] == '@' {
+                neighbors.push(neigh);
+            }
+        }
+
+        if neighbors.len() < 4 {
+            total += 1;
+            grid[point.as_index(grid_size)] = '.';
+            unchecked.extend_from_slice(&neighbors);
+        }
+    }
+
+    total
 }
 
 fn main() -> std::io::Result<()> {
@@ -70,6 +107,11 @@ mod tests {
     #[test]
     fn part1_given_example() {
         assert_eq!(crate::part1(TEXT1), 13);
+    }
+
+    #[test]
+    fn part2_given_example() {
+        assert_eq!(crate::part2(TEXT1), 43);
     }
 
     // #[rstest::rstest]
